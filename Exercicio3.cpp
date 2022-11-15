f1a0a4d939834aec77f6f383c24657d21fbb0719
Diogo Moura <diogomou@gmail.com>
2022-11-15 18:22:25 -0300

    exercicio 3 concluido

--- Exercicio3.cpp
+++ Exercicio3.cpp
@@ -14,14 +14,24 @@ int main()
 
 {
 
-	float emprestimo, juros;
-	int parcelas;
+	float emprestimo, juros,b,c;
+	int parcelas,auxiliar;
 
-	cin>>emprestimo>>juros>>parcelas;
-
-	for(int i = 0)
-
-
-
+	cin>>emprestimo>>parcelas>>juros;
+	auxiliar=0;
+	b=0;
+	c=0;
+	
+	if (emprestimo<1000){cout<<"O valor do emprestimo deve ser > R$ 1000,00"<<endl;}
+	else if (parcelas<10){cout<<"A quantidade de parcelas estar entre 10 e 360."<<endl;}
+	else if (parcelas>360){cout<<"A quantidade de parcelas estar entre 10 e 360."<<endl;}
+	else if(juros<0){cout<<"A taxa de juros deve ser positiva."<<endl;}
+	else{
+		for(int i = 1; i<=parcelas;i++){
+			cout << "Parcela " << i <<" - "<<" R$ "<<setprecision(2) << fixed<< emprestimo/parcelas+((emprestimo-((emprestimo/parcelas)*auxiliar++))*(juros/100))<<endl;
+			c += emprestimo/parcelas+((emprestimo-((emprestimo/parcelas)*b++))*(juros/100));
+		}
+		cout <<"Valor total a ser pago: R$ "<<setprecision(2) << fixed<< c<<endl;
+	}
 }
 
